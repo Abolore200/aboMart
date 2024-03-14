2591e7dab9bd4d37d52c0e5474cde55dd6c9faee
Your Name201~ <abolore200@gmail.com>
2024-03-14 19:06:00 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     // this.display(this.hideMenu)
-    console.log(this.appService.flashSaleCart)
   }
 
   hideMenu: boolean
