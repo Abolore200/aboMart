686ff1d574aeff6ea561393f0bcd019060c16a1b
Your Name201~ <abolore200@gmail.com>
2024-03-15 18:38:08 +0100

    added flash sale section

--- src/app/AppService/app.service.ts
+++ src/app/AppService/app.service.ts
@@ -18,9 +18,9 @@ export class AppService {
   }
 
   flashSaleCart = [
-    {name:'HAVIT HV-G92 Gamepad',price:120,rating:5,image:'assets/images/havit-gamepad.png',review:88,discount:40,old:160},
-    {name:'AK-900 Wired Keyboard',price:960,rating:4,image:'assets/images/ak-keyboard.png',review:75,discount:40,old:1190},
-    {name:'IPS LCD Gaming Monitor',price:370,rating:5,image:'assets/images/ips-gaming-monitor.png',review:99,discount:30,old:400},
-    {name:'S-Series Comfort Chair',price:375,rating:5,image:'assets/images/s-series-chair.png',review:99,discount:25,old:400},
+    {name:'HAVIT HV-G92 Gamepad',price:120,rating:5,image:'assets/images/gamepad.svg',review:88,discount:40,old:160},
+    {name:'AK-900 Wired Keyboard',price:960,rating:4,image:'assets/images/ak900.scg',review:75,discount:40,old:1190},
+    {name:'IPS LCD Gaming Monitor',price:370,rating:5,image:'assets/images/ipsmonitor.svg',review:99,discount:30,old:400},
+    {name:'S-Series Comfort Chair',price:375,rating:5,image:'assets/images/chair.svg',review:99,discount:25,old:400},
   ]
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Subscription, interval, map, takeWhile } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,21 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrl: './home.component.css',
   encapsulation: ViewEncapsulation.None
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  days:number
+  hours:number
+  minutes:number
+  seconds:number
+  ngOnInit(): void {
+    const day = new Date().getDate()
+    const hour = new Date().getHours()
+    const minute = new Date().getMinutes()
+    const second = new Date().getSeconds()
+
+    this.days = day
+    this.hours = hour
+    this.minutes = minute
+    this.seconds = second
+  }
 }
