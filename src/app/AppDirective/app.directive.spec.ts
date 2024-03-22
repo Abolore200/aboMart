729e51c3d3a73623c1a99d98398ba9abb5e3fc05
Your Name201~ <abolore200@gmail.com>
2024-03-22 17:46:57 +0100

    removed errors in ng test

--- src/app/AppDirective/app.directive.spec.ts
+++ src/app/AppDirective/app.directive.spec.ts
@@ -1,8 +1,10 @@
+import { ElementRef, Renderer2 } from '@angular/core';
 import { AppDirective } from './app.directive';
+import { inject } from '@angular/core/testing';
 
 describe('AppDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AppDirective();
+  it('should create an instance', inject([], (element:ElementRef,renderer:Renderer2) => {
+    const directive = new AppDirective(element,renderer);
     expect(directive).toBeTruthy();
-  });
+  }));
 });
